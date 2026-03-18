b70757d96df08c05f4a34c89c796ad8be859fa22
Rakesh04 <rakesh31012004@gmail.com>
2026-03-18 09:48:49 +0530

    update

--- Frontend/src/util/axios.jsx
+++ Frontend/src/util/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "https://airbnb-clone-e05v.onrender.com/api/v1",
+    baseURL: "https://airbnb-clone-e05v.onrender.com/api",
     // baseURL: "http://localhost:4001/api",
     withCredentials: true,
 })
